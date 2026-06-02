ed163b333dc81d7e0439b26ae6da9dd3956125ee
csmig <carlsmigielski@gmail.com>
2026-06-02 16:24:02 -0400

    stug member, volunteer, sr

---
+++ api/source/controllers/Member.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const SmError = require('../utils/error')
+
+module.exports.getMembers = async function getMembers (req, res, next) {
+  try {
+    // TODO: Implement getMembers
+    res.json({})
+  }
+  catch (err) {
+    next(err)
+  }
+}
+
+module.exports.createMember = async function createMember (req, res, next) {
+  try {
+    // TODO: Implement createMember
+    res.json({})
+  }
+  catch (err) {
+    next(err)
+  }
+}
+
+module.exports.getMember = async function getMember (req, res, next) {
+  try {
+    // TODO: Implement getMember
+    res.json({})
+  }
+  catch (err) {
+    next(err)
+  }
+}
+
+module.exports.patchMember = async function patchMember (req, res, next) {
+  try {
+    // TODO: Implement patchMember
+    res.json({})
+  }
+  catch (err) {
+    next(err)
+  }
+}
+
+module.exports.deleteMember = async function deleteMember (req, res, next) {
+  try {
+    // TODO: Implement deleteMember
+    res.json({})
+  }
+  catch (err) {
+    next(err)
+  }
+}
