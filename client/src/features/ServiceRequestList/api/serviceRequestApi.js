43b5a3593870b65ca52de2a65088286c549e245a
csmig <carlsmigielski@gmail.com>
2026-06-16 16:03:41 -0400

    feat: add getServiceRequests API client function

--- client/src/features/ServiceRequestList/api/serviceRequestApi.js
+++ client/src/features/ServiceRequestList/api/serviceRequestApi.js
@@ -5,3 +5,10 @@ export const getVillageServiceRequests = (villageId) =>
 
 export const getServiceRequest = (serviceRequestId, projection = []) =>
   apiCall('getServiceRequest', { serviceRequestId, ...(projection.length && { projection }) })
+
+export const getServiceRequests = ({ status, villageId } = {}) => {
+  const params = {}
+  if (status?.length) params.status = status
+  if (villageId?.length) params.villageId = villageId
+  return apiCall('getServiceRequests', params)
+}
