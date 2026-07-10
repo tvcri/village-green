eb0b3a186a62e85ba9f4d942952b8c51c1f67040
csmig <carlsmigielski@gmail.com>
2026-07-10 17:32:35 -0400

    feat(sr-wallclock): regenerate sql/current schema after migration 0013
    Reflects service_request's new serviceDate/timesFlexible/startTime/
    finishTime/apptTime/returnTime columns (startAt/finishAt dropped), plus
    unrelated staleness already present (capability id 11, migrations 0011-0012).

--- api/source/service/migrations/sql/current/20-vg-static.sql
+++ api/source/service/migrations/sql/current/20-vg-static.sql
@@ -18,7 +18,7 @@
 
 LOCK TABLES `capability` WRITE;
 /*!40000 ALTER TABLE `capability` DISABLE KEYS */;
-INSERT INTO `capability` VALUES (1,'Errands'),(2,'Friends'),(3,'Home Help'),(5,'Rides'),(4,'Tech Support');
+INSERT INTO `capability` VALUES (1,'Errands'),(2,'Friends'),(3,'Home Help'),(5,'Rides'),(11,'Steering Committee'),(4,'Tech Support');
 /*!40000 ALTER TABLE `capability` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -28,7 +28,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `_migrations` WRITE;
 /*!40000 ALTER TABLE `_migrations` DISABLE KEYS */;
-INSERT INTO `_migrations` VALUES ('2026-06-16 17:43:42',NULL,'0001-analytics.js'),('2026-06-22 01:13:42',NULL,'0002-fcv-submission.js'),('2026-06-23 06:26:22',NULL,'0003-service-request-times.js'),('2026-06-23 20:15:07',NULL,'0004-service-request-status-not-null.js'),('2026-06-29 01:59:50',NULL,'0005-notification-event.js'),('2026-06-29 17:19:50',NULL,'0006-ce-member-volunteer-fields.js'),('2026-07-02 20:07:56',NULL,'0007-service-request-attribution.js'),('2026-07-04 02:23:43',NULL,'0008-privacy-acknowledgement.js'),('2026-07-05 15:28:31',NULL,'0009-camelcase-columns.js'),('2026-07-05 15:48:50',NULL,'0010-person-management.js');
+INSERT INTO `_migrations` VALUES ('2026-06-16 17:43:42',NULL,'0001-analytics.js'),('2026-06-22 01:13:42',NULL,'0002-fcv-submission.js'),('2026-06-23 06:26:22',NULL,'0003-service-request-times.js'),('2026-06-23 20:15:07',NULL,'0004-service-request-status-not-null.js'),('2026-06-29 01:59:50',NULL,'0005-notification-event.js'),('2026-06-29 17:19:50',NULL,'0006-ce-member-volunteer-fields.js'),('2026-07-02 20:07:56',NULL,'0007-service-request-attribution.js'),('2026-07-04 02:23:43',NULL,'0008-privacy-acknowledgement.js'),('2026-07-05 15:28:31',NULL,'0009-camelcase-columns.js'),('2026-07-06 14:36:41',NULL,'0010-person-management.js'),('2026-07-06 14:36:41',NULL,'0011-person-disability-note.js'),('2026-07-06 14:36:41',NULL,'0012-volunteer-notes.js'),('2026-07-10 21:27:23',NULL,'0013-sr-wallclock-times.js');
 /*!40000 ALTER TABLE `_migrations` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -39,4 +39,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-07-05 15:48:59
+-- Dump completed on 2026-07-10 21:31:29
